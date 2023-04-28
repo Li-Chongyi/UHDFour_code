d951d265678e516191c5ba6de23aa36dfdda6709
lele53543 <75419309+lele53543@users.noreply.github.com>
2023-04-28 20:26:01 +0800

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,4 +1,4 @@
-CUDA_VISIBLE_DEVICES=3 python src/test_PSNR.py --dataset-name UHD     
-# CUDA_VISIBLE_DEVICES=3 python src/test_PSNR.py --dataset-name LOLv1     
-# CUDA_VISIBLE_DEVICES=3 python src/test_PSNR.py --dataset-name LOLv2
-# CUDA_VISIBLE_DEVICES=3 python src/test_PSNR.py --dataset-name our_test       
+CUDA_VISIBLE_DEVICES=3 python src/test_PSNR.py --dataset-name UHD  --ckpts-dir ./ckpts/UHD_checkpoint.pt  
+# CUDA_VISIBLE_DEVICES=3 python src/test_PSNR.py  --dataset-name LOLv1 --ckpts-dir ./ckpts/LOLv1_checkpoint.pt     
+# CUDA_VISIBLE_DEVICES=3 python src/test_PSNR.py --dataset-name LOLv2 --ckpts-dir ./ckpts/LOLv2_checkpoint.pt
+# CUDA_VISIBLE_DEVICES=3 python src/test_PSNR.py --dataset-name our_test     
